51b49310c316a58bdf69f586f198d8439f17331e
frost-1256 <habanero.iori@gmail.com>
2025-06-13 22:03:33 +0000

    Reactｲｲﾈ!

--- qr-stamprally/app/routes/_index.tsx
+++ qr-stamprally/app/routes/_index.tsx
@@ -2,15 +2,22 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Stamprally" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "スタンプラリー" },
+    { name: "description", content: "スタンプラリー" },
   ];
 };
 
+  const tmp_alert = () => {
+    alert('Clicked!');
+  }
+
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold underline">Hello world!</h1> 
+    <div className="grid justify-items-stretch">
+      <div className="justify-self-center">
+        <h1 className="font-bold underline">Hello world!</h1> 
+    <button onClick={tmp_alert} className="inline-flex h-10 items-center justify-center rounded-md bg-cyan-500 px-6 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-90">押して！</button>
+      </div>
     </div>
   );
 }
