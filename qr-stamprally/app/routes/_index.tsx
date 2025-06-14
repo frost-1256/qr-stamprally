1db836be098ac5e7074f67aebeb611c04b16cf63
frost-1256 <habanero.iori@gmail.com>
2025-06-14 10:19:45 +0000

    わすれあ

--- qr-stamprally/app/routes/_index.tsx
+++ qr-stamprally/app/routes/_index.tsx
@@ -32,13 +32,11 @@ export default function Index() {
   return (
     <div>
       <div className="grid justify-center">
-        <div>
+        <div className="flex flex-col items-center">
           <h1 className="font-bold mt-1 mb-2 grid justify-center">スタンプラリー</h1>
-          <button onClick={handleStamp} className="inline-flex h-10 items-center justify-center rounded-md bg-cyan-500 px-3 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-90">スタンプを押す</button>
-                {/* showQrReaderがtrueの場合のみQrReaderコンポーネントを表示 */}
+          <button onClick={handleStamp} className="mx-auto inline-flex h-10 items-center justify-center justify-self-center-safe rounded-md bg-cyan-500 px-3 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-90">スタンプを押す</button>
+          {/* showQrReaderがtrueの場合のみQrReaderコンポーネントを表示 */}
           {showQrReader && <Qrreader />}
-
-          {!showQrReader}
         </div>
       </div>
       <div className="flex justify-center flex-wrap m-10">
@@ -46,7 +44,7 @@ export default function Index() {
           <div key={stamp.id} className="border p-10 m-3 w-64 bg-cyan-200">
             <h2 className="font-bold">{stamp.name}</h2>
             {showAlert ? (
-             <img src={stamp.stamp_clear} alt={stamp.name} className="rounded-full " />
+              <img src={stamp.stamp_clear} alt={stamp.name} className="rounded-full " />
             ) : (
               <img src={stamp.stamp_img} alt={stamp.name} className="rounded-full " />
             )}
