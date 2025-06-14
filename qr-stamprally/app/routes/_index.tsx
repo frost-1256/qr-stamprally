ba5cdcc7cd4f5153053f1a93099cf3478ec14cb4
frost-1256 <habanero.iori@gmail.com>
2025-06-14 09:25:47 +0000

    空白消した

--- qr-stamprally/app/routes/_index.tsx
+++ qr-stamprally/app/routes/_index.tsx
@@ -26,7 +26,6 @@ export default function Index() {
           <h1 className="font-bold mt-1 mb-2 grid justify-center">スタンプラリー</h1>
           <button onClick={tmp_alert} className="inline-flex h-10 items-center justify-center rounded-md bg-cyan-500 px-3 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-90">スタンプを押す</button>
         </div>
-
       </div>
       <div className="flex justify-center flex-wrap m-10">
         {stamps.map((stamp) => (
