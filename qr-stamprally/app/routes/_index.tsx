1e4e8c4c3335ae66f65d098a3a9349e92739036e
frost-1256 <habanero.iori@gmail.com>
2025-06-14 00:13:14 +0000

    Add stamps.json etc...

--- qr-stamprally/app/routes/_index.tsx
+++ qr-stamprally/app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import stamps from '../stamps.json';
+// スタンプ構成ファイルを読み込み
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,9 +16,12 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="grid justify-center">
-      <div className="">
+      <div >
         <h1 className="font-bold mt-1 mb-2 grid justify-center">スタンプラリー</h1> 
     <button onClick={tmp_alert} className="inline-flex h-10 items-center justify-center rounded-md bg-cyan-500 px-3 font-medium text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-90">スタンプを押す</button>
+      </div>
+      <div>
+
       </div>
     </div>
   );
